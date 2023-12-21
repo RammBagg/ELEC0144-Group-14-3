0debab948f1e944427205ab7b5f256f7b9dc2569
RammBagg <ramit.bag.710@gmail.com>
2023-12-21 21:39:11 +0000

    fix grid looks

--- A-star.py
+++ A-star.py
@@ -89,8 +89,8 @@ def astar(graph, start, end, obstacles):
 
 # Initialize the graph with obstacles
 graph = [
-    [1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 10, 11, 12],
+    [ 1,  2,  3,  4,  5,  6],
+    [ 7,  8,  9, 10, 11, 12],
     [13, 14, 15, 16, 17, 18],
     [19, 20, 21, 22, 23, 24],
     [25, 26, 27, 28, 29, 30],
